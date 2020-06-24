3a325d2a16c0a8eadc01c7079f1989e7b0534d51
Connor Hawley <connor.hawley4@gmail.com>
2020-06-24 19:59:13 -0400

    Add html semantics everywhere needed, configure title, begin opengraph implementation, add favicon

--- components/SiteHead.tsx
+++ components/SiteHead.tsx
@@ -7,5 +7,8 @@ export default () => (
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <link rel="manifest" href="/site.webmanifest" />
     <title>Connor Hawley</title>
+    <meta property="og:type" content="website" />
+    <meta name="description" content="Connor Hawley/'s website and blog" />
+    <meta property="og:title" content="Connor Hawley" />
   </Head>
 )
